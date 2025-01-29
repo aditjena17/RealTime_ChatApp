cc20a809dc0ed9444dcdaacd28bc015e1b7a9ff9
adit <aditjena17@gmail.com>
2025-01-29 12:08:09 +0530

    modified some design changes

---
+++ src/test/java/com/example/chatapp/tests/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatapp.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ChatApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
